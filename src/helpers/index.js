25fc8908e3b16fb149d1bc0cf87994d5146a3c3a
Cedric Poilly <cedpoilly@gmail.com>
2020-02-08 13:02:34 +0400

    New helper: `groupBy`

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -77,3 +77,14 @@ export function groupById(r, a) {
   r[a.id] = a;
   return r;
 }
+
+export function groupBy(objectArray, property) {
+  return objectArray.reduce(function (acc, obj) {
+    var key = obj[property];
+    if (!acc[key]) {
+      acc[key] = [];
+    }
+    acc[key].push(obj);
+    return acc;
+  }, {});
+}
