cdf079b33c0a6eedc79880e11a8ebf60709500d2
Sandeep Ramgolam <sandeep@lsl.digital>
2020-02-08 10:21:51 +0400

    added speaker listing page

---
+++ src/components/speaker-box.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="speaker-box" :id="speaker.fullName">
+		<div class="speaker-image">
+			<img :src="speaker.profilePicture" alt />
+		</div>
+		<div class="speaker-meta">
+			<div class="name">{{ speaker.fullName }}</div>
+			<div class="occupation">{{ speaker.tagLine }}</div>
+			<div class="alias"></div>
+			<div class="talk-count" v-if="speaker.sessions.length > 1">
+				{{
+				speaker.sessions.length }} talks
+			</div>
+			<div class="talk-count" v-else>{{ speaker.sessions.length }} talk</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ["speaker"]
+};
+</script>
--- src/views/PageSpeakers.vue
+++ src/views/PageSpeakers.vue
@@ -1,12 +1,55 @@
 <template>
-	<div class="page-speakers">
-		<h1 class="text-2xl">speakers</h1>
+	<div class="container page-speakers">
+		<h1 class="text-2xl">Speakers</h1>
+		<div class="page-all-speakers page-session">
+			<div class="page-content">
+				<div class="speakers-wrapper" v-if="speakers">
+					<router-link
+						class="speaker-wrapper"
+						v-for="speaker in speakers"
+						:key="speaker.id"
+						:to="{ name: 'speaker', params: { id: speaker.id } }"
+					>
+						<SpeakerBox :speaker="speaker" />
+					</router-link>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+import SpeakerBox from "@/components/speaker-box.vue";
+import { mapActions, mapGetters } from "vuex";
+import { time as timeHelper, getDay as getDayHelper } from "@/helpers";
+
 export default {
-	name: "pagespeakers",
-	components: {}
+	methods: {
+		...mapActions(["FETCH_SESSIONS", "FETCH_SPEAKERS"]),
+		getSpeaker: function(id) {
+			if (this.speakers.length === 0) {
+				this.FETCH_SPEAKERS();
+			}
+			let theSpeaker = this.speakers.find(speaker => speaker.id === id);
+			return theSpeaker.profilePicture;
+		},
+		time: timeHelper,
+		getDay: getDayHelper
+	},
+	computed: {
+		...mapGetters({
+			sessions: "getSessions",
+			speakers: "getSpeakers"
+		})
+	},
+	mounted: function() {
+		this.FETCH_SESSIONS();
+	},
+	components: {
+		SpeakerBox
+	}
 };
 </script>
+
+<style lang="scss" scoped>
+</style>
