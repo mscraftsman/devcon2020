e17d5fd5c78f345daabe1f20a97ed453731acdb8
Cedric Poilly <cedpoilly@gmail.com>
2020-02-08 14:08:26 +0400

    Component to display the list of sponsors in a tier

---
+++ src/components/sponsor-list.vue
@@ -0,0 +1,137 @@
+<template>
+  <div>
+    <h4 class="tier-title">{{tier}}</h4>
+
+    <section class="sponsors-wrapper"
+             :class="getWrapperSize(tier)">
+      <SponsorItem v-for="(sponsor, index) of sponsors"
+                   :key="index"
+                   :sponsor="sponsor" />
+    </section>
+  </div>
+</template>
+
+<script>
+import SponsorItem from "@/components/sponsor-item.vue";
+
+export default {
+  name: "SponsorList",
+
+  props: {
+    sponsors: {
+      type: Array,
+
+      required: true,
+
+      validator(value) {
+        const isValidArray = value.length;
+        return isValidArray;
+      }
+    },
+
+    tier: {
+      type: String,
+
+      required: true,
+
+      validator(value) {
+        return !!value;
+      }
+    }
+  },
+
+  components: {
+    SponsorItem
+  },
+
+  methods: {
+    getWrapperSize(tier) {
+      const MEDIUM_TIERS = ["Speaker Sponsor", "Speaker", "Silver"];
+      const SMALL_TIERS = [
+        "Happy Hour Partner",
+        "Silver",
+        "Bronze",
+        "Internet",
+        "Media Partner",
+        "Day Care"
+      ];
+
+      const medium = MEDIUM_TIERS.find(item => item === tier);
+      const small = SMALL_TIERS.find(item => item === tier);
+
+      let className = "";
+
+      if (medium) {
+        className = "medium";
+      }
+
+      if (small) {
+        className = "small";
+      }
+
+      className = className ? `${className}-wrapper` : "";
+
+      console.log(tier, className);
+      return className;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.sponsor-level-wrapper {
+  // *styling to be defined
+}
+
+.tier-title {
+  // *styling to be defined
+}
+
+.sponsors-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 10px 0 30px;
+  justify-content: center;
+  align-items: center;
+  &:last-child {
+    // border-bottom: 1px solid rgba(#fff, 0.3);
+  }
+}
+
+.medium-wrapper {
+  .sponsor-logo {
+    img {
+      width: 190px;
+    }
+  }
+  // @media screen and (max-width: $tablet) {
+  @media screen and (max-width: 768px) {
+    .sponsor-logo {
+      &:nth-child(even) {
+        padding: 5px;
+      }
+      img {
+        width: 150px;
+      }
+    }
+  }
+}
+.small-wrapper {
+  .sponsor-logo {
+    img {
+      width: 150px;
+    }
+  }
+  // @media screen and (max-width: $tablet) {
+  @media screen and (max-width: 768px) {
+    .sponsor-logo {
+      &:nth-child(even) {
+        padding: 5px;
+      }
+      img {
+        width: 100px;
+      }
+    }
+  }
+}
+</style>
