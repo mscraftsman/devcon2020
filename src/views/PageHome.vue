a6f7172b06f1b98cc7c1067a50d9f90ba974820f
Cedric Poilly <cedpoilly@gmail.com>
2020-02-08 14:24:42 +0400

    Connect the sponsor display to the home page

--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,12 +1,19 @@
 <template>
-	<div class="page-home">
-		<h1 class="text-2xl">oh hi mark!</h1>
-	</div>
+  <div class="page-home">
+    <h1 class="text-2xl">oh hi mark!</h1>
+
+    <HomeSponsorsSection />
+  </div>
 </template>
 
 <script>
+import HomeSponsorsSection from "@/components/home-sponsors-section.vue";
+
 export default {
-	name: "pagehome",
-	components: {}
+  name: "pagehome",
+
+  components: {
+    HomeSponsorsSection
+  }
 };
 </script>
