de9bb88713adb0faa3428076b0dcefac92699e85
Jheengut Pritvi <z.coldplayer@gmail.com>
2020-02-10 23:00:19 +0400

    update src/components/extras/extra-gdg.vue, src/components/extras/extra-lugm.vue, src/components/extras/extra-maker.vue for venue location and date/year.
    added new sections in src/components/extras/extra-gdg.vue to be more consistent with other extra user groups description.

--- src/components/extras/extra-gdg.vue
+++ src/components/extras/extra-gdg.vue
@@ -9,6 +9,13 @@
 				The GDG Mauritius is a local community-ran meetup for developers
 				interested in resources and technology from Google Developers.
 			</p>
+
+                        <p>
+                                The MSCC has an annual event called the Developers Conference which is
+                                free of charge community-driven, IT tech conference hosted annually in
+                                Mauritius. Since 2016 there is an extra venue at the Flying Dodo.
+                        </p>
+
 			<p>
 				GDGs are local groups of developers who are specifically interested in
 				Google products and APIs. Each local group is called a GDG chapter and
@@ -21,6 +28,12 @@
 				and the opinions expressed here should in no way be linked to Google,
 				the corporation.
 			</p>
+
+                        <p>
+                                Come join us at Developers Conference 2020 __ROOM__ of Caudan Arts.
+                        </p>
+
+
 		</div>
 	</div>
 </template>
