8d975b695b1cb455cb1d40748ff6b32f0dfef16e
Sandeep Ramgolam <sandeep@lsl.digital>
2020-02-12 19:26:22 +0400

    image experiments

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -2,7 +2,7 @@
 	<div class="flex min-h-screen flex-col">
 		<!-- <menu-section class="fixed bottom-0"></menu-section> -->
 		<router-view class="flex-grow" />
-		<footer-section />
+		<footer-section class="snap-child-end" />
 		<!-- <div class="spacer h-32 w-full"></div> -->
 	</div>
 </template>
