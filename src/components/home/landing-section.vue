3afe546863357738135fbbac33ed99beadf837e1
Cedric Poilly <cedpoilly@gmail.com>
2020-02-17 23:42:29 +0400

    Update text display

--- src/components/home/landing-section.vue
+++ src/components/home/landing-section.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex items-center justify-center h-screen landing-section">
     <div class="text-box">
-      <div class="flex flex-col items-end justify-between font-bold uppercase">
+      <div class="flex flex-col items-center justify-between font-bold uppercase">
         <div class="text-xl">Caudan Arts Center</div>
         <div class="text-3xl font-black">2 - 4 April 2020</div>
       </div>
-      <h1 class="font-hulksmash font-hairline text-5xl leading-none">Developers Conference 2020</h1>
+      <h1 class="font-hulksmash font-hairline text-5xl leading-none">Developers <span class="text-block">Conference 2020</span></h1>
       <div class="button-wrapper flex justify-around pt-5">
         <h3 class="text-2xl font-black font-spiderman text-gray-800">Registration Opening Soon</h3>
         <!-- <router-link
@@ -25,5 +25,8 @@
   background-size: 160% auto;
   background-position: center center;
   background-repeat: no-repeat;
+}
+.text-block {
+  display: block;
 }
 </style>
