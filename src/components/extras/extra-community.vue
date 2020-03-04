5963b12a0bdccb02c84b25e11ec10ea3572089f6
Cedric Poilly <cedpoilly@gmail.com>
2020-03-04 18:16:16 +0400

    Extra community content update

--- src/components/extras/extra-community.vue
+++ src/components/extras/extra-community.vue
@@ -1,29 +1,20 @@
 <template>
-	<div id="community">
-		<div>
-			<div class="tagline">Communication and Exchange with Others</div>
-			<h2 class="group-title">Networking & Community</h2>
-		</div>
-		<div>
-			<p>
-				Communication with your environment is an essential part of everyone's
-				life. And it doesn't matter whether you are actually living in a rural
-				area in the middle of nowhere, within the pulsating heart of a big city,
-				or in my case on a wonderful island in the Indian Ocean. The ability to
-				exchange your thoughts, your experience and your worries with another
-				person helps you to get different points of view and new ideas on how to
-				resolve an issue you might be confronted with.
-			</p>
-			<p>
-				Take the chance to exchange with other attendees in the reception area
-				or on the balcony. Have some interesting conversations about software
-				development, latest information gained during one of the presentations
-				and let others know about your ups & downs in your projects.
-			</p>
-			<p>
-				Also, join the Mauritius Software Craftsmanship Community and be part of
-				the local IT community.
-			</p>
-		</div>
-	</div>
+  <div id="community">
+    <div>
+      <div class="tagline">Communication and Exchange with Others</div>
+      <h2 class="group-title">Networking & Community</h2>
+    </div>
+    <div>
+      <p>
+        Take the chance to exchange with other attendees, have some interesting
+        conversations about software development, latest information gained
+        during one of the presentations and let others know about your ups &amp;
+        downs in your projects.
+      </p>
+      <p>
+        The coffee breaks and Happy Hour are just the right moments to maybe
+        make a new friend. Think about it.
+      </p>
+    </div>
+  </div>
 </template>
