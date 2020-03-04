0157cc160d5c31a4e3e902fcc61cc4445f70f653
Cedric Poilly <cedpoilly@gmail.com>
2020-03-04 18:39:28 +0400

    Community updates

--- src/components/extras/extra-community.vue
+++ src/components/extras/extra-community.vue
@@ -5,16 +5,21 @@
       <h2 class="group-title">Networking & Community</h2>
     </div>
     <div>
-      <p>
-        Take the chance to exchange with other attendees, have some interesting
+      <p class="my-3">
+        Take the chance to exchange with other attendees, speakers and partners, have some interesting
         conversations about software development, latest information gained
         during one of the presentations and let others know about your ups &amp;
         downs in your projects.
       </p>
-      <p>
-        The coffee breaks and Happy Hour are just the right moments to maybe
-        make a new friend. Think about it.
+      <p class="my-3">
+        Our Partners are here for existing & new talents for either recruitment or internship or simply for a chit chat. Make the most out of it!
+        Our Happy Hour is just the right moment to maybe
+        make a new friend, seal some deals in a more relaxed atmosphere. Think about it.
+      </p>
+      <p class="my-3">
+        But above all, come and enjoy this vibrant atmosphere full of technology & activities!
       </p>
     </div>
   </div>
 </template>
+
